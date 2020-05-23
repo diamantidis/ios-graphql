6edd5220213a666a1ab3d9c743d78f2f89acee94
Ioannis Diamantidis <diamantidis@outlook.com>
2020-05-23 08:28:18 +0200

    Initial Commit

---
+++ iOSGraphQL/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iOSGraphQL
+//
+//  Created by Ioannis Diamantidis on 5/23/20.
+//  Copyright © 2020 Ioannis Diamantidis. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
